5b2e4ba8ed44ed5acdf0b94603ee6d44029045e5
Sphynx <153016991+Sphynx-code@users.noreply.github.com>
2023-12-27 16:30:13 +0500

    Added files via upload

---
+++ peoplecounter.js
@@ -0,0 +1,23 @@
+let countdisplay = document.getElementById("countrender");
+let count = 0;
+
+let saverender = document.getElementById("save-el");
+
+
+
+function increment(){
+    count += 1;
+    countdisplay.textContent = count;
+    
+}
+function save(){
+    savestr = count + " - ";;
+    saverender.textContent += savestr; 
+    countdisplay.textContent = 0;
+
+
+}
+
+
+
+    
